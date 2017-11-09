b0c3a8fec07eb7423a3abeb32a9bc44cfca234b1
Darren R Rabick <31315974+drabick@users.noreply.github.com>
2017-11-09 09:22:42 -0600

    Added new map title
    Added map title.

--- js/gmaps.js
+++ js/gmaps.js
@@ -14,15 +14,14 @@ function initMap() {
   icon: 'pin.png'
   });
         
-var contentString = '<h1>Chicago Street Pinball Arcade</h1><p>There are few things more truly American than pinball. Did you know that over 90% of the worlds pinball machines have been designed and manufactured in the Chicago area?  Its true.  And is still true to this day.Pinball is something that can only approximated, and never duplicated.  No two games are ever the same, and there arent patterns to memorize to guarantee you the high score.Pinball is a completely visceral experience, immersing and saturating the senses, which is something smart phones and consoles just cant do.Come visit us in historic Downtown Joliet, a block off of old Route 66, for your chance to partake in pinball for the first time, or the thousandth time.  Either way, youll be drawn back to the lure of the mystical silver ball.</p>';
+  var contentString = '<h1>Chicago Street Pinball Arcade</h1><p>There are few things more truly American than pinball. Did you know that over 90% of the worlds pinball machines have been designed and manufactured in the Chicago area?  Its true.  And is still true to this day.Pinball is something that can only approximated, and never duplicated.  No two games are ever the same, and there arent patterns to memorize to guarantee you the high score.Pinball is a completely visceral experience, immersing and saturating the senses, which is something smart phones and consoles just cant do.Come visit us in historic Downtown Joliet, a block off of old Route 66, for your chance to partake in pinball for the first time, or the thousandth time.  Either way, youll be drawn back to the lure of the mystical silver ball.</p>';
 
-var infowindow = new google.maps.InfoWindow({
-  content: contentString
- 	});        
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+    });        
                 
-google.maps.event.addListener(marker, 'click', function() {
-infowindow.open(map, marker);  	
-    });
-        
-        
-      }      
+  google.maps.event.addListener(marker, 'click', function() {
+  infowindow.open(map, marker);  	
+  });
+                
+}      
