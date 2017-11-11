6dd99faa7fcb81302956ee1cadc144227b8d0b1d
Darren R Rabick <31315974+drabick@users.noreply.github.com>
2017-11-11 13:59:41 -0600

    Cleaned up white space in HTML and JS
    Removed uneeded white spaces in HTML and JS files.

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,9 +1,8 @@
-//-- Goofle Mao Place MaRKER API Jscript -->
-      
+//-- Goofle Mao Place MaRKER API Jscript -->  
 function initMap() {
   var myLatLng = {lat: 41.526899, lng: -88.078976};
-          
-  var aboutstring = '<h1>Chicago Street Pinball Arcade</h1><p>There are few things more truly American than pinball. Did you know that over 90% of the worlds pinball' + 
+
+  var aboutstring = '<h1>Chicago Street Pinball Arcade</h1><p>There are few things more truly American than pinball. Did you know that over 90% of the worlds pinball' +
   ' machines have been designed and manufactured in the Chicago area?  Its true.  And is still true to this day.Pinball is something that can only approximated, and never duplicated.' +
   '  No two games are ever the same, and there arent patterns to memorize to guarantee you the high score.Pinball is a completely visceral experience, immersing and saturating the senses,'+
   ' which is something smart phones and consoles just cant do.Come visit us in historic Downtown Joliet, a block off of old Route 66, for your chance to partake in pinball for the first time,'+
@@ -11,8 +10,8 @@ function initMap() {
 
   var infowindow = new google.maps.InfoWindow({
     content: aboutstring
-    });   
-    
+    });
+
   var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 15,
   center: myLatLng
@@ -28,9 +27,9 @@ function initMap() {
     map: map,
     icon: 'pin.png'
   });
-  
+
   google.maps.event.addListener(marker, 'click', function() {
-  infowindow.open(map, marker);  	
+  infowindow.open(map, marker);
   });
-                
-}      
+
+}
